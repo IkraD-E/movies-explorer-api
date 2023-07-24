0e604dde14f1aea713af50e5226f49cbee74ae27
Ikra-D <kino0.512321@gmail.com>
2023-07-24 12:56:06 +0800

    Add files

---
+++ app.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const routerIndex = require('./routes/index');
+const errorHandler = require('./middlewares/errorHandler');
+const NotFound = require('./errors/NotFound');
+const cors = require('./middlewares/cors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+// Working port
+
+const { PORT = 3000 } = process.env;
+
+// Database URL
+
+const BASE_URL = 'mongodb://127.0.0.1:27017/mestodb';
+
+const app = express();
+
+mongoose
+  .connect(BASE_URL)
+  .then(() => {
+    console.log(`Успешно подключен к серверу: ${BASE_URL}`);
+  })
+  .catch(() => {
+    console.log(`Провалено подключение к серверу: ${BASE_URL}`);
+  });
+
+app.use(cors);
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(requestLogger);
+
+// Server Crash test
+
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
+
+app.use('/', routerIndex);
+
+app.use((req, res, next) => {
+  next(new NotFound('Страница не найдена. Где вы взяли на неё ссылку?'));
+});
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Слушаем порт: ${PORT}`);
+});
---
+++ controllers/movies.js
@@ -0,0 +1,53 @@
+const Movie = require('../models/movie');
+const MissiedData = require('../errors/MissiedData');
+const NotFound = require('../errors/NotFound');
+const BadRequest = require('../errors/BadRequest');
+
+module.exports.createMovie = (req, res, next) => {
+  const data = {
+    ...req.body,
+    owner: req.user._id,
+  };
+
+  Movie
+    .create(data)
+    .then((movie) => res.status(201).send(movie))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequest('Введены некорректные данные'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.getMovies = (req, res, next) => {
+  Movie
+    .find({})
+    .populate(['owner', 'likes'])
+    .then((movies) => res.send(movies))
+    .catch(next);
+};
+
+module.exports.deleteMovie = (req, res, next) => {
+  Movie
+    .findById(req.params.movieId)
+    .orFail(() => next(new NotFound('Карточка не найдена')))
+    .populate(['owner'])
+    .then((movie) => {
+      if (!(req.user._id === String(movie.owner._id))) {
+        next(new MissiedData('Эта карточка принадлежит другому пользователю'));
+      } else {
+        Movie.findByIdAndRemove(req.params.movieId)
+          .then((deletedMovie) => res.send(deletedMovie))
+          .catch(next);
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequest('Некорректные данные карточки'));
+      } else {
+        next(err);
+      }
+    });
+};
