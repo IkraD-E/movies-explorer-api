21b3988344d6bad2feae0c98db8e59ff223d3702
Pavel Evtushenko <pasha.512321@gmail.com>
2023-09-08 02:35:19 +0800

    some changes

--- routes/users.js
+++ routes/users.js
@@ -15,12 +15,6 @@ routesUsers.get('/me', getUserMe);
 
 routesUsers.delete(
   '/me',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ru'] } }),
-      password: Joi.string().required().min(8),
-    }),
-  }),
   logout,
 );
 
--- utils/config.js
+++ utils/config.js
@@ -9,7 +9,7 @@ const {
 
 const DEV_SECRET = 'SUPERSECRETKEY';
 const DEV_DATABASE_URL = 'mongodb://localhost:27017/mydatabase';
-const DEV_PORT = 3000;
+const DEV_PORT = 3001;
 
 const DB_URL = NODE_ENV === 'production' && DATABASE_URL ? DATABASE_URL : DEV_DATABASE_URL;
 
